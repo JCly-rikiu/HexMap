503528df29c36e80ab55fb9e0f9459379edac939
JCly-rikiu <jcly.rikiu@gmail.com>
2019-11-06 11:46:44 +0800

    finish Fusing Edges

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -40,6 +40,6 @@ public static class HexMetrics
 
     public static Vector3 GetBridge(HexDirection direction)
     {
-        return (corners[(int)direction] + corners[(int)direction + 1]) * 0.5f * blendFactor;
+        return (corners[(int)direction] + corners[(int)direction + 1]) * blendFactor;
     }
 }
