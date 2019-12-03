79d558f5a1dd476c2bd831f54582221296a3060c
JCly-rikiu <jcly.rikiu@gmail.com>
2019-12-03 19:31:09 +0800

    finish More Shore Water

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -644,14 +644,14 @@ public class HexGridChunk : MonoBehaviour
 
     void TriangulateOpenWater(HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center)
     {
-        Vector3 c1 = center + HexMetrics.GetFirstSolidCorner(direction);
-        Vector3 c2 = center + HexMetrics.GetSecondSolidCorner(direction);
+        Vector3 c1 = center + HexMetrics.GetFirstWaterCorner(direction);
+        Vector3 c2 = center + HexMetrics.GetSecondWaterCorner(direction);
 
         water.AddTriangle(center, c1, c2);
 
         if (direction <= HexDirection.SE && neighbor != null)
         {
-            Vector3 bridge = HexMetrics.GetBridge(direction);
+            Vector3 bridge = HexMetrics.GetWaterBridge(direction);
             Vector3 e1 = c1 + bridge;
             Vector3 e2 = c2 + bridge;
 
@@ -665,21 +665,22 @@ public class HexGridChunk : MonoBehaviour
                     return;
                 }
 
-                water.AddTriangle(c2, e2, c2 + HexMetrics.GetBridge(direction.Next()));
+                water.AddTriangle(c2, e2, c2 + HexMetrics.GetWaterBridge(direction.Next()));
             }
         }
     }
 
     void TriangulateWaterShore(HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center)
     {
-        EdgeVertices e1 = new EdgeVertices(center + HexMetrics.GetFirstSolidCorner(direction), center + HexMetrics.GetSecondSolidCorner(direction));
+        EdgeVertices e1 = new EdgeVertices(center + HexMetrics.GetFirstWaterCorner(direction), center + HexMetrics.GetSecondWaterCorner(direction));
         water.AddTriangle(center, e1.v1, e1.v2);
         water.AddTriangle(center, e1.v2, e1.v3);
         water.AddTriangle(center, e1.v3, e1.v4);
         water.AddTriangle(center, e1.v4, e1.v5);
 
-        Vector3 bridge = HexMetrics.GetBridge(direction);
-        EdgeVertices e2 = new EdgeVertices(e1.v1 + bridge, e1.v5 + bridge);
+        Vector3 center2 = neighbor.Position;
+        center2.y = center.y;
+        EdgeVertices e2 = new EdgeVertices(center2 + HexMetrics.GetSecondSolidCorner(direction.Opposite()), center2 + HexMetrics.GetFirstSolidCorner(direction.Opposite()));
         waterShore.AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
         waterShore.AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
         waterShore.AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
@@ -692,7 +693,9 @@ public class HexGridChunk : MonoBehaviour
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
         if (nextNeighbor != null)
         {
-            waterShore.AddTriangle(e1.v5, e2.v5, e1.v5 + HexMetrics.GetBridge(direction.Next()));
+            Vector3 v3 = nextNeighbor.Position + (nextNeighbor.IsUnderwater ? HexMetrics.GetFirstWaterCorner(direction.Previous()) : HexMetrics.GetFirstSolidCorner(direction.Previous()));
+            v3.y = center.y;
+            waterShore.AddTriangle(e1.v5, e2.v5, v3);
             waterShore.AddTriangleUV(new Vector2(0f, 0f), new Vector2(0f, 1f), new Vector2(0f, nextNeighbor.IsUnderwater ? 0f : 1f));
         }
     }
--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -23,6 +23,8 @@ public static class HexMetrics
 
     public const float streamBedElevationOffset = -1.75f;
     public const float waterElevationOffset = -0.5f;
+    public const float waterFactor = 0.6f;
+    public const float waterBlendFactor = 1f - waterFactor;
 
     public const int chunkSizeX = 5, chunkSizeZ = 5;
 
@@ -112,5 +114,20 @@ public static class HexMetrics
         position.z += (sample.z * 2f - 1f) * cellPerturbStrength;
 
         return position;
+    }
+
+    public static Vector3 GetFirstWaterCorner(HexDirection direction)
+    {
+        return corners[(int)direction] * waterFactor;
+    }
+
+    public static Vector3 GetSecondWaterCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1] * waterFactor;
+    }
+
+    public static Vector3 GetWaterBridge(HexDirection direction)
+    {
+        return (corners[(int)direction] + corners[(int)direction + 1]) * waterBlendFactor;
     }
 }
