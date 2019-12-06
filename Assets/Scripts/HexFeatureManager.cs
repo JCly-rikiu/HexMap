b2efcd561223b8b117aa49f8a7f57ccbfead6659
JCly-rikiu <jcly.rikiu@gmail.com>
2019-12-07 06:47:46 +0800

    finish Avoiding Cliffs and Water

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -86,7 +86,7 @@ public class HexFeatureManager : MonoBehaviour
 
     public void AddWall(EdgeVertices near, HexCell nearCell, EdgeVertices far, HexCell farCell, bool hasRiver, bool hasRoad)
     {
-        if (nearCell.Walled != farCell.Walled)
+        if (nearCell.Walled != farCell.Walled && !nearCell.IsUnderwater && !farCell.IsUnderwater && nearCell.GetEdgeType(farCell) != HexEdgeType.Cliff)
         {
             AddWallSegment(near.v1, far.v1, near.v2, far.v2);
             if (hasRiver || hasRoad)
@@ -178,7 +178,40 @@ public class HexFeatureManager : MonoBehaviour
 
     void AddWallSegment(Vector3 pivot, HexCell pivotCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell)
     {
-        AddWallSegment(pivot, left, pivot, right);
+        if (pivotCell.IsUnderwater)
+        {
+            return;
+        }
+
+        bool hasLeftWall = !leftCell.IsUnderwater && pivotCell.GetEdgeType(leftCell) != HexEdgeType.Cliff;
+        bool hasRightWall = !rightCell.IsUnderwater && pivotCell.GetEdgeType(rightCell) != HexEdgeType.Cliff;
+
+        if (hasLeftWall)
+        {
+            if (hasRightWall)
+            {
+                AddWallSegment(pivot, left, pivot, right);
+            }
+            else if (leftCell.Elevation < rightCell.Elevation)
+            {
+                AddWallWedge(pivot, left, right);
+            }
+            else
+            {
+                AddWallCap(pivot, left);
+            }
+        }
+        else if (hasRightWall)
+        {
+            if (rightCell.Elevation < leftCell.Elevation)
+            {
+                AddWallWedge(right, pivot, left);
+            }
+            else
+            {
+                AddWallCap(right, pivot);
+            }
+        }
     }
 
     void AddWallCap(Vector3 near, Vector3 far)
@@ -195,5 +228,27 @@ public class HexFeatureManager : MonoBehaviour
         v2 = v4 = center + thickness;
         v3.y = v4.y = center.y + HexMetrics.wallHeight;
         walls.AddQuadUnperturbed(v1, v2, v3, v4);
+    }
+
+    void AddWallWedge(Vector3 near, Vector3 far, Vector3 point)
+    {
+        near = HexMetrics.Perturb(near);
+        far = HexMetrics.Perturb(far);
+        point = HexMetrics.Perturb(point);
+
+        Vector3 center = HexMetrics.WallLerp(near, far);
+        Vector3 thickness = HexMetrics.WallThicknessOffset(near, far);
+
+        Vector3 v1, v2, v3, v4;
+        Vector3 pointTop = point;
+        point.y = center.y;
+
+        v1 = v3 = center - thickness;
+        v2 = v4 = center + thickness;
+        v3.y = v4.y = pointTop.y = center.y + HexMetrics.wallHeight;
+
+        walls.AddQuadUnperturbed(v1, point, v3, pointTop);
+        walls.AddQuadUnperturbed(point, v2, pointTop, v4);
+        walls.AddTriangleUnperturbed(pointTop, v3, v4);
     }
 }
