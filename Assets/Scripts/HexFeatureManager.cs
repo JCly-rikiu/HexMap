e5b37b6490a7de5c3f2e0f1316ab424e3f0c3091
JCly-rikiu <jcly.rikiu@gmail.com>
2019-12-07 02:14:28 +0800

    finish Walls on Terraces

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -88,7 +88,10 @@ public class HexFeatureManager : MonoBehaviour
     {
         if (nearCell.Walled != farCell.Walled)
         {
-            AddWallSegment(near.v1, far.v1, near.v5, far.v5);
+            AddWallSegment(near.v1, far.v1, near.v2, far.v2);
+            AddWallSegment(near.v2, far.v2, near.v3, far.v3);
+            AddWallSegment(near.v3, far.v3, near.v4, far.v4);
+            AddWallSegment(near.v4, far.v4, near.v5, far.v5);
         }
     }
 
@@ -131,8 +134,13 @@ public class HexFeatureManager : MonoBehaviour
 
     void AddWallSegment(Vector3 nearLeft, Vector3 farLeft, Vector3 nearRight, Vector3 farRight)
     {
-        Vector3 left = Vector3.Lerp(nearLeft, farLeft, 0.5f);
-        Vector3 right = Vector3.Lerp(nearRight, farRight, 0.5f);
+        nearLeft = HexMetrics.Perturb(nearLeft);
+        farLeft = HexMetrics.Perturb(farLeft);
+        nearRight = HexMetrics.Perturb(nearRight);
+        farRight = HexMetrics.Perturb(farRight);
+
+        Vector3 left = HexMetrics.WallLerp(nearLeft, farLeft);
+        Vector3 right = HexMetrics.WallLerp(nearRight, farRight);
 
         Vector3 leftThicknessOffset = HexMetrics.WallThicknessOffset(nearLeft, farLeft);
         Vector3 rightThicknessOffset = HexMetrics.WallThicknessOffset(nearRight, farRight);
@@ -145,7 +153,7 @@ public class HexFeatureManager : MonoBehaviour
         v2 = v4 = right - rightThicknessOffset;
         v3.y = leftTop;
         v4.y = rightTop;
-        walls.AddQuad(v1, v2, v3, v4);
+        walls.AddQuadUnperturbed(v1, v2, v3, v4);
 
         // wall tops
         Vector3 t1 = v3, t2 = v4;
@@ -154,10 +162,10 @@ public class HexFeatureManager : MonoBehaviour
         v2 = v4 = right + rightThicknessOffset;
         v3.y = leftTop;
         v4.y = rightTop;
-        walls.AddQuad(v2, v1, v4, v3);
+        walls.AddQuadUnperturbed(v2, v1, v4, v3);
 
         // wall tops
-        walls.AddQuad(t1, t2, v3, v4);
+        walls.AddQuadUnperturbed(t1, t2, v3, v4);
     }
 
     void AddWallSegment(Vector3 pivot, HexCell pivotCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell)
--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -28,6 +28,7 @@ public static class HexMetrics
 
     public const float wallHeight = 3f;
     public const float wallThickness = 0.75f;
+    public const float wallElevationOffset = verticalTerraceStepSize;
 
     public const int chunkSizeX = 5, chunkSizeZ = 5;
 
@@ -183,5 +184,14 @@ public static class HexMetrics
         offset.y = 0f;
         offset.z = far.z - near.z;
         return offset.normalized * (wallThickness * 0.5f);
+    }
+
+    public static Vector3 WallLerp(Vector3 near, Vector3 far)
+    {
+        near.x += (far.x - near.x) * 0.5f;
+        near.z += (far.z - near.z) * 0.5f;
+        float v = near.y < far.y ? wallElevationOffset : (1f - wallElevationOffset);
+        near.y += (far.y - near.y) * v;
+        return near;
     }
 }
