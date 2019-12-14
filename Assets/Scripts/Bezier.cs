64c57f1a08d8f274255d4fcc4f3c172b02c96bae
JCly-rikiu <jcly.rikiu@gmail.com>
2019-12-14 10:29:17 +0800

    finish Flowing Movement

---
+++ Assets/Scripts/Bezier.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class Bezier
+{
+    public static Vector3 GetPoint(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        float r = 1f - t;
+        return r * r * a + 2f * r * t * b + t * t * c;
+    }
+}
--- Assets/Scripts/HexUnit.cs
+++ Assets/Scripts/HexUnit.cs
@@ -58,14 +58,24 @@ public class HexUnit : MonoBehaviour
             return;
         }
 
+        Vector3 a, b, c = pathToTravel[0].Position;
         for (int i = 1; i < pathToTravel.Count; i++)
         {
-            Vector3 a = pathToTravel[i - 1].Position;
-            Vector3 b = pathToTravel[i].Position;
+            a = c;
+            b = pathToTravel[i - 1].Position;
+            c = (b + pathToTravel[i].Position) * 0.5f;
             for (float t = 0f; t < 1f; t += 0.1f)
             {
-                Gizmos.DrawSphere(Vector3.Lerp(a, b, t), 2f);
+                Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
             }
+        }
+
+        a = c;
+        b = pathToTravel[pathToTravel.Count - 1].Position;
+        c = b;
+        for (float t = 0f; t < 1f; t += 0.1f)
+        {
+            Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
         }
     }
 
@@ -90,16 +100,31 @@ public class HexUnit : MonoBehaviour
 
     IEnumerator TravelPath()
     {
+        Vector3 a, b, c = pathToTravel[0].Position;
+        float t = Time.deltaTime * travelSpeed;
         for (int i = 1; i < pathToTravel.Count; i++)
         {
-            Vector3 a = pathToTravel[i - 1].Position;
-            Vector3 b = pathToTravel[i].Position;
-            for (float t = 0f; t < 1f; t += Time.deltaTime * travelSpeed)
+            a = c;
+            b = pathToTravel[i - 1].Position;
+            c = (b + pathToTravel[i].Position) * 0.5f;
+            for (; t < 1f; t += Time.deltaTime * travelSpeed)
             {
-                transform.localPosition = Vector3.Lerp(a, b, t);
+                transform.localPosition = Bezier.GetPoint(a, b, c, t);
                 yield return null;
             }
+            t -= 1f;
         }
+
+        a = c;
+        b = pathToTravel[pathToTravel.Count - 1].Position;
+        c = b;
+        for (; t < 1f; t += Time.deltaTime * travelSpeed)
+        {
+            transform.localPosition = Bezier.GetPoint(a, b, c, t);
+            yield return null;
+        }
+
+        transform.localPosition = location.Position;
     }
 
     public void Save(BinaryWriter writer)
