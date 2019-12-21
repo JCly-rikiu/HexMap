f7e44647721a6bfa3910e79b37473868930f5227
JCly-rikiu <jcly.rikiu@gmail.com>
2019-12-21 23:06:47 +0800

    finish Creating Land

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -5,12 +5,90 @@ public class HexMapGenerator : MonoBehaviour
 {
     public HexGrid grid;
 
+    int cellCount;
+
+    HexCellPriorityQueue searchFrontier;
+    int searchFrontierPhase;
+
+    [Range(0f, 0.5f)]
+    public float jitterProbability = 0.25f;
+    [Range(20, 200)]
+    public int chunkSizeMin = 30;
+    [Range(20, 200)]
+    public int chunkSizeMax = 100;
+    [Range(5, 95)]
+    public int landPercentage = 50;
+
     public void GenerateMap(int x, int z)
     {
+        cellCount = x * z;
         grid.CreateMap(x, z);
-        for (int i = 0; i < z; i++)
+        if (searchFrontier == null)
         {
-            grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+            searchFrontier = new HexCellPriorityQueue();
         }
+
+        CreateLand();
+
+        for (int i = 0; i < cellCount; i++)
+        {
+            grid.GetCell(i).SearchPhase = 0;
+        }
+    }
+
+    void CreateLand()
+    {
+        int landBudget = Mathf.RoundToInt(cellCount * landPercentage * 0.01f);
+        while (landBudget > 0)
+        {
+            landBudget = RaiseTerrain(Random.Range(chunkSizeMin, chunkSizeMax + 1), landBudget);
+        }
+    }
+
+    int RaiseTerrain(int chunkSize, int budget)
+    {
+        searchFrontierPhase += 1;
+
+        HexCell firstCell = GetRandomCell();
+        firstCell.SearchPhase = searchFrontierPhase;
+        firstCell.Distance = 0;
+        firstCell.SearchHeuristic = 0;
+        searchFrontier.Enqueue(firstCell);
+        HexCoordinates center = firstCell.coordinates;
+
+        int size = 0;
+        while (size < chunkSize && searchFrontier.Count > 0)
+        {
+            HexCell current = searchFrontier.Dequeue();
+            if (current.TerrainTypeIndex == 0)
+            {
+                current.TerrainTypeIndex = 1;
+                if (--budget == 0)
+                {
+                    break;
+                }
+            }
+            size += 1;
+
+            for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
+            {
+                HexCell neighbor = current.GetNeighbor(d);
+                if (neighbor && neighbor.SearchPhase < searchFrontierPhase)
+                {
+                    neighbor.SearchPhase = searchFrontierPhase;
+                    neighbor.Distance = neighbor.coordinates.DistanceTo(center);
+                    neighbor.SearchHeuristic = Random.value < jitterProbability ? 1 : 0;
+                    searchFrontier.Enqueue(neighbor);
+                }
+            }
+        }
+        searchFrontier.Clear();
+
+        return budget;
+    }
+
+    HexCell GetRandomCell()
+    {
+        return grid.GetCell(Random.Range(0, cellCount));
     }
 }
